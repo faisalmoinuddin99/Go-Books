83f68166bd7f521a7ce0c71c6abc3ebca645008c
faisal <faisal25march99@gmail.com>
2020-11-19 11:30:27 +0530

    Soultion 2

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
 Excercise:
- Go as a calculator. Write a program that computes 32,132 × 42,452 and
- prints it to the terminal (use the * operator for multiplication).
+ (true && false) || (false && true) || !
+(false && false)?
 */
 
 package main
@@ -9,5 +9,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("32,132 × 42,452 = ", 32132*42452)
+	fmt.Println((true && false) || (false && true) || !(false && false))
 }
