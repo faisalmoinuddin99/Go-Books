ee5068f46574bb342ca9fc81a7d9e65fc5afa02a
faisal <faisal25march99@gmail.com>
2020-11-19 10:03:03 +0530

    Multiline Comments and double qutoes

--- helloworld.go
+++ helloworld.go
@@ -5,6 +5,12 @@ import "fmt"
 // "fmt" stands for "format", implements input and output
 // this is comment
 
+/*
+Notice that fmt is surrounded by double quotes.
+The use of double quotes like this is known as a string literal, which is
+a type of expression. In Go, strings represent a sequence of characters
+(letters, numbers, symbols, etc.) of a definite length.
+*/
 func main() {
 	fmt.Println("Hello, World !")
 }
