2ba72278adc63fbbc5b0c7ebd705c4a6dd8f01de
faisal <faisal25march99@gmail.com>
2020-11-19 10:05:37 +0530

    thid commit ( Function Declartion and Statement )

--- helloworld.go
+++ helloworld.go
@@ -11,6 +11,8 @@ The use of double quotes like this is known as a string literal, which is
 a type of expression. In Go, strings represent a sequence of characters
 (letters, numbers, symbols, etc.) of a definite length.
 */
+
+// Function Declartion
 func main() {
-	fmt.Println("Hello, World !")
+	fmt.Println("Hello, World !") // statement
 }
