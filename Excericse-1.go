92fb13d22c04a1001486e15f08714672dcf5a31c
faisal <faisal25march99@gmail.com>
2020-11-19 10:19:19 +0530

    Done With Excercise 1

---
+++ Excericse-1.go
@@ -0,0 +1,13 @@
+/*
+Modify the program we wrote so that instead of printing Hello, World it
+prints.
+Hello, my name is followed by your name.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Faisal !")
+}
