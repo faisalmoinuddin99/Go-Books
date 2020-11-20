ec696f57942baff20ceb1f984e34da8a788e6658
faisal <faisal25march99@gmail.com>
2020-11-20 10:27:47 +0530

    Assigning a value is optional, so we could use two statements, like this:

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var x string = " Hello world !"
-	fmt.Println(x)
+	// var x string = " Hello world !"
+	// fmt.Println(x)
 
-	var y string = "Hello faisal"
-	fmt.Println(y)
+	// var y string = "Hello faisal"
+	// fmt.Println(y)
+	// Also
+
+	var x string
+	x = "Hello world !"
+	fmt.Println(x)
 }
