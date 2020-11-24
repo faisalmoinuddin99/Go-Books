ec57deff898212d45fe7bcfabc608125f1186e7d
faisal <faisal25march99@gmail.com>
2020-11-24 16:33:51 +0530

    Excersise 1

--- main.go
+++ main.go
@@ -3,42 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		Switch Case:
-		WAP to that printed the English names for numbers.
-	*/
 
-	var i int16
-	i = 1
-	for i <= 6 {
-		switch i {
-		case 0:
-			fmt.Print("Zero")
-			break
-		case 1:
-			fmt.Print("One")
-			break
-		case 2:
-			fmt.Print("Two")
-			break
-		case 3:
-			fmt.Print("Three")
-			break
-		case 4:
-			fmt.Print("Four")
-			break
-		case 5:
-			fmt.Print("Five")
-			break
-		case 6:
-			fmt.Print("Six")
-			break
-		default:
-			fmt.Print("unknown number")
-			break
-
-		}
-
-		i++
+	i := 10
+	if i > 10 {
+		fmt.Println("Big")
+	} else {
+		fmt.Println("Small")
 	}
+
 }
