47b98a0ea11e5d9d56b38ac199d99b4468f6d5b0
faisal <faisal25march99@gmail.com>
2020-11-25 07:07:09 +0530

    Updated for loop in array

--- main.go
+++ main.go
@@ -15,10 +15,14 @@ func main() {
 	marks[3] = 70
 	marks[4] = 81
 	var total float32
-	var i = 0
-	for ; i < 5; i++ {
-		total += marks[i]
+
+	for _, value := range marks {
+		total += value
 	}
+	/*
+		A single underscore (_) is used to tell the compiler that we
+		don’t need this (in this case, we don’t need the iterator variable).
+	*/
 	fmt.Print("Total marks =", total)
 	fmt.Println("Average marks =", total/float32(len(marks))) // TypeCasting
 }
