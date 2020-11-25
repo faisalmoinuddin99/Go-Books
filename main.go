9fd5811de4e61774837f74b149c592451d71a43d
faisal <faisal25march99@gmail.com>
2020-11-25 07:19:15 +0530

    Shorter hand syntax for creating an array

--- main.go
+++ main.go
@@ -1,28 +1,21 @@
+/*
+Shorter hand syntax for creating an array
+*/
+
 package main
 
 import "fmt"
 
 func main() {
-	/*
-		WAP to find the average of total marks
-	*/
-
-	var marks [5]float32
-
-	marks[0] = 98
-	marks[1] = 89
-	marks[2] = 85
-	marks[3] = 70
-	marks[4] = 81
-	var total float32
-
-	for _, value := range marks {
-		total += value
+	x := [5]float64{
+		98,
+		99,
+		100,
+		121,
+		45,
 	}
-	/*
-		A single underscore (_) is used to tell the compiler that we
-		don’t need this (in this case, we don’t need the iterator variable).
-	*/
-	fmt.Print("Total marks =", total)
-	fmt.Println("Average marks =", total/float32(len(marks))) // TypeCasting
+
+	for _, value := range x {
+		fmt.Print(value, " ")
+	}
 }
