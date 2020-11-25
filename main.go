89c7aa28c137e1836d4530b05a01f2ca7494e901
faisal <faisal25march99@gmail.com>
2020-11-25 06:46:52 +0530

    Arrays

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Chapter 5 : Arrays, Slices, and Maps")
+	var array_name [5]int
+	array_name[4] = 100
+
+	fmt.Print(array_name)
 }
+
+/*
+You should see this:
+[0 0 0 0 100]
+
+x[4] = 100 should be read “set the fifth element of the array x to 100.”
+It might seem strange that x[4] represents the fifth element instead
+of the fourth, but like strings, arrays are indexed starting from 0.
+Arrays are accessed in a similar way. We could change fmt.Println(x)
+to fmt.Println(x[4]) and we would get 100.
+*/
