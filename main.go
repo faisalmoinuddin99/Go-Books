e42aa9d51260a37814a668ba2c27b66c7a2f7477
faisal <faisal25march99@gmail.com>
2020-11-29 18:57:29 +0530

    ch-5 excercise question 1

--- main.go
+++ main.go
@@ -8,27 +8,6 @@ import "fmt"
 
 func main() {
 
-	// make new
-	// new - only allocates - no init memory
-	// make - allocates and init memory - non zerored storage
-	score := make(map[string]int)
-	score["faisal"] = 90
-	fmt.Println(score)
-
-	score["hitesh"] = 78
-	score["rahul"] = 60
-	score["nikky"] = 58
-	score["ron"] = 98
-
-	for key, value := range score {
-		fmt.Printf("Score of %v is %v \n", key, value)
-	}
-
-	delete(score, "nikky")
-	for key, value := range score {
-		fmt.Printf("Score of %v is %v \n", key, value)
-	}
-
-	getFaisalScore := score["faisal"]
-	fmt.Println(getFaisalScore)
+	x := [6]string{"a", "b", "c", "d", "e", "f"}
+	fmt.Print(x[1:5])
 }
