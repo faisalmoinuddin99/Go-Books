f8795b4a635a72717f40ce63d20d281a847ca350
faisal <faisal25march99@gmail.com>
2020-11-29 19:25:24 +0530

    Write a program that finds the smallest number in this list: x := []int{ 48,96,86,68, 57,82,63,70, 37,34,83,27, 19,97, 9,17, }
    Soultion

--- main.go
+++ main.go
@@ -8,6 +8,24 @@ import "fmt"
 
 func main() {
 
-	x := [6]string{"a", "b", "c", "d", "e", "f"}
-	fmt.Print(x[1:5])
+	//Write a program that finds the smallest number in this list:
+
+	list := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	//var sizeOfList = len(list)
+
+	var min = 0
+	min = list[0]
+	fmt.Println("Value of min before loop :", min)
+	for _, value := range list {
+		if value < min {
+			min = value
+		}
+
+	}
+	fmt.Println("Value of min after loop :", min)
 }
